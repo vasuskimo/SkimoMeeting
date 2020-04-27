b1f6565e41ae43806237261555126f126b3e4b53
vasuskimo <vasu@skimo.tv>
2020-04-26 20:22:10 -0700

    changed path of ffmpeg

--- scripts/skimo.sh
+++ scripts/skimo.sh
@@ -1,4 +1,4 @@
 asset="$1"
 assetid="$2"
 cwd="$3"
-/usr/local/bin/ffprobe -show_frames -hide_banner -v quiet -print_format compact -f lavfi "movie=$asset,select=gt(scene\,.3)" | egrep -o "pkt_pts_time=[0-9.]+" | cut -d'=' -f2- > $cwd/public/$assetid/timecodes.txt
+/usr/bin/ffprobe -show_frames -hide_banner -v quiet -print_format compact -f lavfi "movie=$asset,select=gt(scene\,.3)" | egrep -o "pkt_pts_time=[0-9.]+" | cut -d'=' -f2- > $cwd/public/$assetid/timecodes.txt
