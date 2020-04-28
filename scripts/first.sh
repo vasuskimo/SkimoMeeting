08708b18e61d5dec756c800ffa55a27858f52ca5
vasuskimo <vasu@skimo.tv>
2020-04-28 10:47:54 -0700

    changed pom to include actuator

--- scripts/first.sh
+++ scripts/first.sh
@@ -1,4 +1,4 @@
 asset="$1"
 assetid="$2"
 cwd="$3"
-/usr/local/bin/ffmpeg -i $asset  -v quiet -filter:v "select='eq(n\,1)',showinfo" -vsync 0 $cwd/public/$assetid/img/poster.jpg &
+/usr/bin/ffmpeg -i $asset  -v quiet -filter:v "select='eq(n\,1)',showinfo" -vsync 0 $cwd/public/$assetid/img/poster.jpg &
--- scripts/thumbnails.sh
+++ scripts/thumbnails.sh
@@ -1,4 +1,4 @@
 asset="$1"
 assetid="$2"
 cwd="$3"
-/usr/local/bin/ffmpeg -i $asset  -v quiet -filter:v "select='gt(scene,0.3)',showinfo" -vsync 0 $cwd/public/$assetid/img/frames%d.jpg &
+/usr/bin/ffmpeg -i $asset  -v quiet -filter:v "select='gt(scene,0.3)',showinfo" -vsync 0 $cwd/public/$assetid/img/frames%d.jpg &
