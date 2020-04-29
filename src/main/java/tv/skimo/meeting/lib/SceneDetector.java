ce43f1452c29ac3d927cd6025e4e51327488fcf7
vasuskimo <vasu@skimo.tv>
2020-04-28 23:46:43 -0700

    added ability to upload file while the Skimo Engine is busy

--- src/main/java/tv/skimo/meeting/lib/SceneDetector.java
+++ src/main/java/tv/skimo/meeting/lib/SceneDetector.java
@@ -4,14 +4,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import tv.skimo.meeting.background.Scheduler;
  
 public class SceneDetector
 { 
+	private static final Logger log = LoggerFactory.getLogger(Scheduler.class);
+
 	public static void generateFirst(String asset, String assetId)
 	{
         String cwd = System.getProperty("user.dir");
         
-        String [] command = {cwd + "/scripts/first.sh", cwd + "/upload-dir/" + asset, assetId, cwd};     
+        String [] command = {cwd + "/scripts/first.sh", cwd  + "/" + asset, assetId, cwd};  
+        
+		log.info("generate first " + Arrays.toString(command));
         
         ProcessBuilder processBuilder = new ProcessBuilder(command); 
         processBuilder.directory(new File(System.getProperty("user.home")));
@@ -30,7 +40,10 @@ public class SceneDetector
 	{
         String cwd = System.getProperty("user.dir");
         
-        String [] command = {cwd + "/scripts/thumbnails.sh", cwd + "/upload-dir/" + asset, assetId, cwd};
+        String [] command = {cwd + "/scripts/thumbnails.sh", cwd + "/" + asset, assetId, cwd};
+        
+		log.info("generate thumbnail " + Arrays.toString(command));
+
         
         ProcessBuilder processBuilder = new ProcessBuilder(command); 
         processBuilder.directory(new File(System.getProperty("user.home")));
@@ -50,7 +63,13 @@ public class SceneDetector
 	{
         String cwd = System.getProperty("user.dir");
         
-        String [] command = {cwd + "/scripts/skimo.sh", cwd + "/upload-dir/" + asset, assetId, cwd};
+        log.info("cwd is " + cwd);
+        log.info("assetId " + assetId);
+        log.info("asset is "  + asset);
+        
+        String [] command = {cwd + "/scripts/skimo.sh", cwd + "/" + asset, assetId, cwd};
+        
+		log.info("generate skimo " + Arrays.toString(command));
         
         ProcessBuilder processBuilder = new ProcessBuilder(command); 
         processBuilder.directory(new File(System.getProperty("user.home")));
