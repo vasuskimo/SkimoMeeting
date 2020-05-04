3d95765b4da27999b7a856709d29a0c6a61045ec
vasuskimo <vasu@skimo.tv>
2020-05-04 15:18:45 -0700

    reverted back to hardcoding

--- scripts/skimo.sh
+++ scripts/skimo.sh
@@ -1,10 +1,5 @@
 asset="$1"
 assetid="$2"
 cwd="$3"
-if [[ $(uname -s) == "Linux" ]]
-then
-    script="/usr/bin/ffprobe"
-else
-    script="/usr/local/bin/ffprobe"
-fi
+script="/usr/bin/ffprobe"
 $script -show_frames -hide_banner -v quiet -print_format compact -f lavfi "movie=$asset,select=gt(scene\,.3)" | egrep -o "pkt_pts_time=[0-9.]+" | cut -d'=' -f2- > $cwd/public/$assetid/timecodes.txt
