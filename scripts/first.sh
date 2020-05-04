3a1d034dd604773fb93a5b80aba88355c64a92a7
vasuskimo <vasu@skimo.tv>
2020-05-04 15:18:54 -0700

    reverted back to hardcoding

--- scripts/first.sh
+++ scripts/first.sh
@@ -1,10 +1,5 @@
 asset="$1"
 assetid="$2"
 cwd="$3"
-if [[ $(uname -s) == "Linux" ]]
-then
-    script="/usr/bin/ffmpeg"
-else
-    script="/usr/local/bin/ffmpeg"
-fi
+script="/usr/bin/ffmpeg"
 $script -i $asset  -v quiet -filter:v "select='eq(n\,1)',showinfo" -vsync 0 $cwd/public/$assetid/img/poster.jpg &
