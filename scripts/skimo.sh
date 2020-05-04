96fe5b9cc4ba0d991bc7c7791436099f2343f0a8
vasuskimo <vasu@skimo.tv>
2020-05-04 13:15:07 -0700

    made the scripts os independent

--- scripts/skimo.sh
+++ scripts/skimo.sh
@@ -1,4 +1,10 @@
 asset="$1"
 assetid="$2"
 cwd="$3"
-/usr/bin/ffprobe -show_frames -hide_banner -v quiet -print_format compact -f lavfi "movie=$asset,select=gt(scene\,.3)" | egrep -o "pkt_pts_time=[0-9.]+" | cut -d'=' -f2- > $cwd/public/$assetid/timecodes.txt
+if [[ $(uname -s) == Linux ]]
+then
+    script="/usr/bin/ffprobe"
+else
+    script="/usr/local/bin/ffprobe"
+fi
+$script -show_frames -hide_banner -v quiet -print_format compact -f lavfi "movie=$asset,select=gt(scene\,.3)" | egrep -o "pkt_pts_time=[0-9.]+" | cut -d'=' -f2- > $cwd/public/$assetid/timecodes.txt
