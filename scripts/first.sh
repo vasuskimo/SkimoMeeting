654f6e41e4a4c032e7aed8be752334c8e0058c79
vasuskimo <vasu@skimo.tv>
2020-05-04 13:15:03 -0700

    made the scripts os independent

--- scripts/first.sh
+++ scripts/first.sh
@@ -1,4 +1,10 @@
 asset="$1"
 assetid="$2"
 cwd="$3"
-/usr/bin/ffmpeg -i $asset  -v quiet -filter:v "select='eq(n\,1)',showinfo" -vsync 0 $cwd/public/$assetid/img/poster.jpg &
+if [[ $(uname -s) == Linux ]]
+then
+    script="/usr/bin/ffmpeg"
+else
+    script="/usr/local/bin/ffmpeg"
+fi
+$script -i $asset  -v quiet -filter:v "select='eq(n\,1)',showinfo" -vsync 0 $cwd/public/$assetid/img/poster.jpg &
