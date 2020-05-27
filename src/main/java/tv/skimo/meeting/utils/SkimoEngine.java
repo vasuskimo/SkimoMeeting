49b20e769c557654d7e028cae06a4f5b2143b837
vasuskimo <vasu@skimo.tv>
2020-05-26 21:47:06 -0700

    skimo engine

--- src/main/java/tv/skimo/meeting/utils/SkimoEngine.java
+++ src/main/java/tv/skimo/meeting/utils/SkimoEngine.java
@@ -4,10 +4,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
  
 public class SkimoEngine
 { 
@@ -83,6 +91,48 @@ public class SkimoEngine
 			log.error("Threw an exception in SkimoEngine::Detect Scene, full stack trace follows:", e);
         }
 	}
+	
+	   public static String getOCR(String asset, String tessData)
+	   {
+		   //log.debug("Getting OCR for " + asset);
+		   File image = new File(asset);
+		   Tesseract tesseract = new Tesseract();
+		   tesseract.setDatapath(tessData);
+		   tesseract.setLanguage("eng");
+		   String result = "";
+		   try 
+		   {
+			   result = tesseract.doOCR(image);
+		   } 
+		   catch (TesseractException e) 
+		   {
+			   e.printStackTrace();
+		   }
+		   result = result.replace("\n", "").replaceAll("[\\W]|_", "").toLowerCase();
+		   int len = result.split(System.getProperty("line.separator")).length;
+		   //log.debug("The image " + asset +  " has " + len + " lines and " + result.length() + " characters");
+		   //log.info(result);
+		   return result;
+	   }
+	   
+	   public static ArrayList<String> getTextFromImage(String assetId) 
+	   {
+		   ArrayList<String> text = new ArrayList<String>();
+	       String cwd = System.getProperty(Constants.USER_DIR) + "/" + Constants.PUBLIC + assetId + Constants.IMG_DIR;       
+	       Path dir = Paths.get(cwd);
+	       
+	       try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.jpg")) 
+	       {
+	           for (Path entry: stream) 
+	           {
+	             text.add(getOCR(cwd + entry.getFileName().toString(), Constants.TESS_DATA));
+	           }
+	       } catch (Exception ex) 
+	       {
+	    	   log.error("exception thrown by Tesseract go method" + ex);
+	       }
+	       return text;
+	   }
 
 	
     public static void main(String[] args) 
