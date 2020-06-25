6149e1da7894f4f5b589b9241706997abe8245b2
vasuskimo <vasu@skimo.tv>
2020-06-24 18:10:02 -0700

    added closed captioning

--- src/main/java/tv/skimo/engine/SkimoEngine.java
+++ src/main/java/tv/skimo/engine/SkimoEngine.java
@@ -10,12 +10,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import tv.skimo.meeting.lib.SRTParser;
 import tv.skimo.meeting.utils.Constants;
  
 public class SkimoEngine
@@ -93,9 +95,47 @@ public class SkimoEngine
         }
 	}
 	
-	   public static String getOCR(String asset, String tessData)
-	   {
-		   //log.debug("Getting OCR for " + asset);
+	public static void generateSRT(String asset, String assetId)
+	{
+        String cwd = System.getProperty(Constants.USER_DIR);
+        String srt = assetId + ".srt";
+        
+        String [] command = {cwd + "/scripts/subtitlegenerate.sh", cwd + Constants.ROOT + asset, assetId, cwd, srt};
+        
+		log.info("generate SRT " + Arrays.toString(command));
+   
+        ProcessBuilder processBuilder = new ProcessBuilder(command); 
+        processBuilder.directory(new File(System.getProperty(Constants.USER_HOME)));
+        
+        try 
+        {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader (new InputStreamReader(process.getInputStream()));
+            Thread.sleep(10000);
+    		SRTParser s = new SRTParser();
+    		String srtFile = System.getProperty(Constants.USER_DIR) + "/" + Constants.PUBLIC + assetId + "/" + assetId + ".srt";
+    		String subFile = System.getProperty(Constants.USER_DIR) + "/" + Constants.PUBLIC + assetId + "/" + assetId + ".sub";
+    		HashMap<String, String> hMap = s.process(srtFile);
+    		s.writeToFile(hMap,subFile);
+        } 
+        catch (Exception e) 
+        {
+			log.error("Threw an exception in SkimoEngine::generateSRT, full stack trace follows:", e);
+        }
+	}
+	
+	public static void generateSub(String assetId)
+	{
+		SRTParser s = new SRTParser();
+		String srtFile = System.getProperty(Constants.USER_DIR) + "/" + Constants.PUBLIC + assetId + "/" + assetId + ".srt";
+		String subFile = System.getProperty(Constants.USER_DIR) + "/" + Constants.PUBLIC + assetId + "/" + assetId + ".sub";
+		HashMap<String, String> hMap = s.process(srtFile);
+		s.writeToFile(hMap,subFile);
+	}
+
+    public static String getOCR(String asset, String tessData)
+    {
+		   log.info("Getting OCR for " + asset);
 		   File image = new File(asset);
 		   Tesseract tesseract = new Tesseract();
 		   tesseract.setDatapath(tessData);
@@ -111,29 +151,28 @@ public class SkimoEngine
 		   }
 		   result = result.replace("\n", "").replaceAll("[\\W]|_", "").toLowerCase();
 		   int len = result.split(System.getProperty("line.separator")).length;
-		   //log.debug("The image " + asset +  " has " + len + " lines and " + result.length() + " characters");
-		   //log.info(result);
 		   return result;
-	   }
+    }
 	   
-	   public static ArrayList<String> getTextFromImage(String assetId) 
-	   {
-		   ArrayList<String> text = new ArrayList<String>();
-	       String cwd = System.getProperty(Constants.USER_DIR) + "/" + Constants.PUBLIC + assetId + Constants.IMG_DIR;       
-	       Path dir = Paths.get(cwd);
+    public static ArrayList<String> getTextFromImage(String assetId) 
+	{
+    	ArrayList<String> text = new ArrayList<String>();
+	    String cwd = System.getProperty(Constants.USER_DIR) + "/" + Constants.PUBLIC + assetId + Constants.IMG_DIR;       
+	    Path dir = Paths.get(cwd);
 	       
-	       try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.jpg")) 
-	       {
-	           for (Path entry: stream) 
-	           {
-	             text.add(getOCR(cwd + entry.getFileName().toString(), Constants.TESS_DATA));
-	           }
-	       } catch (Exception ex) 
-	       {
-	    	   log.error("exception thrown by Tesseract go method" + ex);
-	       }
-	       return text;
-	   }
+	    try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.jpg")) 
+	    {
+           for (Path entry: stream) 
+           {
+             text.add(getOCR(cwd + entry.getFileName().toString(), Constants.TESS_DATA));
+           }
+	    } 
+	    catch (Exception ex) 
+	    {
+	    	log.error("exception thrown by Tesseract go method" + ex);
+	    }
+	    return text;
+	}
 
 	
     public static void main(String[] args) 
