e8046f69db7cb267bad0aedea7c894f467171289
vasuskimo <vasu@skimo.tv>
2020-09-10 17:31:41 -0700

    warnings removed

--- src/main/java/tv/skimo/meeting/controller/SkimoMeetingController.java
+++ src/main/java/tv/skimo/meeting/controller/SkimoMeetingController.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
