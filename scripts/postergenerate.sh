b9bb0bf7f4564c3c4cb89f9c8eb5eade6846f172
vasuskimo <vasu@skimo.tv>
2020-10-08 14:22:15 -0700

    correct version

--- scripts/postergenerate.sh
+++ scripts/postergenerate.sh
@@ -1,5 +1,5 @@
 asset="$1"
 assetid="$2"
 cwd="$3"
-script="/usr/local/bin/ffmpeg"
+script="/usr/bin/ffmpeg"
 $script -i $asset  -v quiet -filter:v "select='eq(n\,1)',showinfo" -vsync 0 $cwd/public/$assetid/img/poster.jpg &
--- scripts/subtitlegenerate.sh
+++ scripts/subtitlegenerate.sh
@@ -2,5 +2,5 @@ asset="$1"
 assetid="$2"
 cwd="$3"
 srt="$4"
-script="/usr/local/bin/ffmpeg"
+script="/usr/bin/ffmpeg"
 $script -i $asset $cwd/public/$assetid/$srt
