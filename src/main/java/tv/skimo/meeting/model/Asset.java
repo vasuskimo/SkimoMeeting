01b6c81ea362095d99f3a2d3653dacf14e1a2f3f
vasuskimo <vasu@skimo.tv>
2020-12-17 17:01:07 -0800

    db support

---
+++ src/main/java/tv/skimo/meeting/model/Asset.java
@@ -0,0 +1,51 @@
+package tv.skimo.meeting.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import tv.skimo.meeting.utils.Status;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+public class Asset 
+{
+ 	
+  @Id
+  @GeneratedValue
+  private Long id;
+  
+  @Column(unique=true)
+  private String assetId;
+  
+  private String name;
+  
+  private String duration;
+  
+  private String email;
+  
+  private String asset_url;
+  
+  @Column(nullable = false, updatable = false)
+  @CreationTimestamp
+  private Timestamp createdOn;
+    
+  private Timestamp end;
+  
+  @Enumerated(EnumType.ORDINAL)
+  private Status status = Status.ACTIVE;
+
+  @Enumerated(EnumType.ORDINAL)
+  private AssetType assetType = AssetType.PUBLIC;
+    
+}
